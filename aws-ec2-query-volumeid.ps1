49f51efa3e73af1d8a36ee5870959a90caeb3b1a
Stephen Cole <stephenjcole@msn.com>
2018-04-22 22:44:32 -0700

    Update aws-ec2-query-volumeid.ps1

--- aws-ec2-query-volumeid.ps1
+++ aws-ec2-query-volumeid.ps1
@@ -1,9 +1,10 @@
-# This will create a query based on the VolumeId of Amazon EBS volume
+# Use PowerShell to get the Volume ID
 
 $INSTANCE = (aws ec2 describe-instances --filter 'Name=tag:Name,Values=EC2-INSTANCE-NAME') -join "" | ConvertFrom-Json
 $VOL_ID = $INSTANCE.Reservations.Instances.BlockDeviceMappings.Ebs.VolumeId
 $INSTANCE_ID = $INSTANCE.Reservations.Instances.InstanceId
 $INSTANCE.Reservations.Instances.BlockDeviceMappings.Ebs.VolumeId
 
+AWS CLI Equivalent:
+
 aws ec2 describe-instances --filter 'Name=tag:Name,Values=EC2-INSTANCE-NAME' --query 'Reservations[0].Instances[0].BlockDeviceMappings[0].Ebs.{VolumeId:VolumeId}'
- 
