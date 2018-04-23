f9518ed9778d34cb8d3ff7ec62a22ce6b6f3b347
Stephen Cole <stephenjcole@msn.com>
2018-04-22 22:26:07 -0700

    Update aws-ec2-stop-instance.ps1

--- aws-ec2-stop-instance.ps1
+++ aws-ec2-stop-instance.ps1
@@ -1,3 +1,5 @@
+# This will stop an instance tagged with a Name of EC2-INSTANCE-NAME.
+
 $INSTANCE = (aws ec2 describe-instances --filter 'Name=tag:Name,Values=EC2-INSTANCE-NAME') -join "" | ConvertFrom-Json
 $VOL_ID = $INSTANCE.Reservations.Instances.BlockDeviceMappings.Ebs.VolumeId
 $INSTANCE_ID = $INSTANCE.Reservations.Instances.InstanceId
